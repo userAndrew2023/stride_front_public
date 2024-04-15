29c3a340d9a90177fb169ce29721ee88ed647cd5
andrew777f <asdfdjfdnmcdk@yandex.by>
2024-04-15 13:45:47 +0300

    initial commit

---
+++ src/components/HeaderActionIcons.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header_action_icons">
+    <span class="material-symbols-outlined header_action_icon">search</span>
+    <span class="material-symbols-outlined header_action_icon">account_circle</span>
+    <span class="material-symbols-outlined header_action_icon">shopping_bag</span>
+  </div>
+</template>
+
+<style scoped>
+.header_action_icons {
+  width: 150px;
+  display: flex;
+  justify-content: space-between;
+}
+.header_action_icon {
+  cursor: pointer;
+  font-size: 30px;
+  color: var(--color-grey);
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
